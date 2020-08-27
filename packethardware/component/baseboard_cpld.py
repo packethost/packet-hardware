51fb093cccc7b786eadcbd6e91b51d2d3db85e83
Dustin Miller <dustin@packet.com>
2020-08-26 19:30:04 -0700

    cpld: add missing properties

--- packethardware/component/baseboard_cpld.py
+++ packethardware/component/baseboard_cpld.py
@@ -6,13 +6,13 @@ class BaseboardCPLD(Component):
     def __init__(self):
         Component.__init__(self)
 
-        self.data = {}
 
-        self.name = "Unknown"
-        self.vendor = "Unknown"
-        self.model = "Unknown"
-        self.serial = "Unknown"
+        self.name = "CPLD"
+        self.vendor = utils.dmidecode_string("system-manufacturer")
+        self.model = utils.dmidecode_string("system-product-name")
+        self.serial = utils.dmidecode_string("system-uuid")
         self.firmware_version = utils.get_baseboard_cpld("firmware_version")
+        self.data = {}
 
     @classmethod
     def list(cls, _):
