b9878d2df9d0311076d5c9d58f8853dc4bc8a7a8
Mike Mason <mason@packet.com>
2020-10-01 09:59:50 -0500

    cleanup formatting

--- packethardware/component/management_controller.py
+++ packethardware/component/management_controller.py
@@ -6,7 +6,6 @@ class ManagementController(Component):
     def __init__(self):
         Component.__init__(self)
 
-
         self.model = utils.dmidecode_string("system-product-name")
         self.firmware_version = utils.get_mc_info("firmware_version")
 
@@ -14,7 +13,7 @@ class ManagementController(Component):
             try:
                 self.model = utils.get_fru_info("Board Part Number")
                 self.name = utils.dmidecode_string("system-product-name")
-            except:
+            except Exception:
                 utils.log(message="Something went wrong, probably no ipmitool.")
                 self.firmware_version = ""
         elif self.model == "AS -1114S-WTRT":
@@ -22,7 +21,7 @@ class ManagementController(Component):
                 self.model = utils.get_fru_info("Board Part Number")
                 self.name = utils.dmidecode_string("system-product-name")
                 self.firmware_version = utils.get_mc_info("firmware_version")
-            except:
+            except Exception:
                 utils.log(message="Something went wrong, probably no ipmitool.")
                 self.firmware_version = ""
         else:
